07750f9f71e535b22cd37bda7b83c330049189cc
salmanAliMemon <memon.salmanali97@gmail.com>
2023-12-21 01:33:30 +0500

    handle api response

--- src/index.js
+++ src/index.js
@@ -15,7 +15,13 @@
 import dotenv from 'dotenv'
 
 import {connectDatabase} from './db/index.js' 
+import {app} from './app.js'
 
 dotenv.config({path : './env'})
 
-connectDatabase()
+connectDatabase().then(()=>{
+    app.listen(`${process.env.PORT}` ?? 8080 , ()=>{
+        console.log(`app is running on port ${process.env.PORT}`)
+    })
+}).catch((err)=> console.log(err)
+)
